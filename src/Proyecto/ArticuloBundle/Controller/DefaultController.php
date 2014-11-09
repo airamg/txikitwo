cd29907bb13d3a9e4012bbf23bc9b92387f8130f
Maria <mariagpdo@gmail.com>
2014-11-09 17:17:47 +0100

    New structure divs and information menu twigs

--- src/Proyecto/ArticuloBundle/Controller/DefaultController.php
+++ src/Proyecto/ArticuloBundle/Controller/DefaultController.php
@@ -8,11 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ArticuloBundle:Default:personalizacion.html.twig');
-    }
-
-    public function busquedaindexAction()
-    {
-        return $this->render('ArticuloBundle:Busqueda:busqueda_index.html.twig');
+        return $this->render('ArticuloBundle:Default:index.html.twig');
     }
 }
