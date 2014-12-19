782ad58c18db7a13ffb3f889c24308ea2d3e801a
Maria <mariagpdo@gmail.com>
2014-12-19 17:06:50 +0100

    Compra form completed

--- src/Proyecto/CompraBundle/Controller/DefaultController.php
+++ src/Proyecto/CompraBundle/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Proyecto\PersonalizacionBundle\Entity\Personalizacion;
 use Proyecto\PersonalizacionBundle\Entity\PersonalizacionRepository;
 use Proyecto\UsuarioBundle\Entity\Usuario;
 use Proyecto\UsuarioBundle\Entity\UsuarioRepository;
+use Proyecto\CompraBundle\Entity\Compra;
+use Proyecto\CompraBundle\Entity\CompraRepository;
 
 class DefaultController extends Controller
 {
@@ -77,24 +79,16 @@ class DefaultController extends Controller
         ));
     }
 
-    public function pedidoAction($id)
+    public function pedidoAction($id, $precio)
     {
+        $request = $this->getRequest();
         $em = $this->getDoctrine()->getManager();
 
-        //SIN HACER
-        //CAMBIAR PARAMETRO PENDIENTE POR REALIZADA PERSONALIZACION AL ENVIAR FORM
-        //comprobar que el id sea distinto de 0, si es igual a 0 es que se hace el pedido de todas las compras pendientes y guardar todas las compras (hacer for)
-        //si es distinto de 0: buscar la personalizacion por id para cambiar el pendiente a 0 y guardar la compra
-        //buscar usuario segun el id de la personalizacion (hacer metodo en repositry) o coger el online hecho debajo
-
-        //buscar usuario online
+        //buscar usuario online y numero de personalizaciones pendientes
         $online = 0;
         $usuario = $em->getRepository('UsuarioBundle:Usuario')->findUserOnline();
-
-        //buscar las compras pendientes que tiene el usuario online
         $num = 0;
         $personalizacion = $em->getRepository('PersonalizacionBundle:Personalizacion')->findPendientesByEmailUsuario($usuario->getEmail());
-
         if(!$usuario) {
             $num = 0;
         } else {
@@ -103,13 +97,52 @@ class DefaultController extends Controller
                 $num = $num + 1;
             }
         }
+        //buscar las personalizaciones correspondientes segun el id
+        $foundpersonalizacion = $em->getRepository('PersonalizacionBundle:Personalizacion')->findById($id);
+
+        if ($request->isMethod('POST')) {
+            if($id == "0")
+            {
+                //cambiar personalizaciones pendientes
+                foreach ($personalizacion as $pendiente) {
+                    $pendiente->setPendiente(0);
+                    $em->persist($pendiente);
+                    $em->flush();
+                    //crear las compras correspondientes a cada personalizacion
+                    $compra1 = new Compra();
+                    //guardo el precio total de la compra completa y no el individual de cada personalizacion
+                    $compra1->setPrecio($precio);
+                    $compra1->setUsuario($pendiente->getUsuario());
+                    $compra1->setPersonalizacion($pendiente);
+                    $em->persist($compra1);
+                    $em->flush();
+                }
+            }
+            else
+            {
+                $foundpersonalizacion->setPendiente(0);
+                $em->persist($foundpersonalizacion);
+                $em->flush();
+                //crear las compras correspondientes a cada personalizacion
+                $compra2 = new Compra();
+                $compra2->setPrecio($precio);
+                $compra2->setUsuario($foundpersonalizacion->getUsuario());
+                $compra2->setPersonalizacion($foundpersonalizacion);
+                $em->persist($compra2);
+                $em->flush();
+            }
+
+            return $this->redirect($this->generateUrl('web_homepage'));
+        }
 
         return $this->render('CompraBundle:Default:pedido.html.twig', array(
             'personalizacion' => $personalizacion,
             'num' => $num,
             'online' => $online,
-            'id' => $id
+            'id' => $id,
+            'foundpersonalizacion' => $foundpersonalizacion,
+            'precio' => $precio,
+            'usuario' => $usuario
         ));
     }
-
 }
